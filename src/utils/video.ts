b7ad8639db995b9048848ab54080c59e88371e2b
Róbert Klimant <roboklimant@gmail.com>
2024-02-12 01:40:01 +0100

    feat: video links load

---
+++ src/utils/video.ts
@@ -0,0 +1,12 @@
+export const video = () => {
+  const getLinks = document.querySelectorAll('[video-link]');
+
+  getLinks.forEach((elLink) => {
+    const linkValue = elLink.getAttribute('video-link');
+    const thumbnailLinkValue = elLink.getAttribute('thumbnail-link');
+
+    const videoEl = `<div data-poster-url="${thumbnailLinkValue}" data-video-urls="${linkValue}" data-autoplay="true" data-loop="true" data-wf-ignore="true" class="home_work_video w-background-video w-background-video-atom"><video id="83f4c1bc-8db0-a962-e80e-cebf1cde13a7-video" autoplay="" loop="" style="background-image:url(&quot;${thumbnailLinkValue}&quot;)" muted="" playsinline="" data-wf-ignore="true" data-object-fit="cover"><source src="${linkValue}" data-wf-ignore="true"></video></div>`;
+
+    elLink.innerHTML = videoEl;
+  });
+};
