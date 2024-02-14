1320c5d2d17384408a0d90443c8da68aea091b9a
Róbert Klimant <roboklimant@gmail.com>
2024-02-14 03:30:58 +0100

    fix: animation fix on breakpoints

--- src/utils/animation.ts
+++ src/utils/animation.ts
@@ -9,22 +9,59 @@ export const animation = () => {
 
   const headerEl = document.querySelector('.home_header_list');
   if (!headerEl) return;
-  let tween = gsap.fromTo(
-    headerEl,
-    {
-      y: 0,
-    },
-    {
-      y: -200,
-      ease: 'none',
-      scrollTrigger: {
-        trigger: headerEl,
-        start: 'bottom 30%',
-        end: 'bottom 0%',
-        scrub: true,
+
+  function scrollAnimation(start: string, end: string) {
+    let tween = gsap.fromTo(
+      headerEl,
+      {
+        y: 0,
       },
+      {
+        y: -200,
+        ease: 'none',
+        scrollTrigger: {
+          trigger: headerEl,
+          // start: 'bottom 30%',
+          // end: 'bottom 0%',
+          start: start,
+          end: end,
+          scrub: true,
+        },
+      }
+    );
+  }
+
+  gsap.matchMedia().add(
+    {
+      isPortrait: '(max-width: 467px)',
+      isTablet: '(max-width: 1149px)',
+      isDesktop: '(max-width: 1920px)',
+      isDesktopX: '(max-width: 2440px)',
+      isDesktopXL: '(min-width: 2441px)',
+    },
+    (context) => {
+      let { isPortrait, isTablet, isDesktop, isDesktopX, isDesktopXL } = context.conditions;
+      if (isPortrait) {
+        scrollAnimation('bottom 10%', 'bottom -5%');
+      } else if (isTablet) {
+        scrollAnimation('bottom 10%', 'bottom 0%');
+      } else if (isDesktop) {
+        scrollAnimation('bottom 30%', 'bottom 0%');
+      } else if (isDesktopX) {
+        scrollAnimation('bottom 35%', 'bottom 0%');
+      } else if (isDesktopXL) {
+        scrollAnimation('bottom 35%', 'bottom 0%');
+      }
     }
   );
+
+  // gsap.matchMedia().add('(max-width: 2440px)', () => {
+  //   scrollAnimation('bottom 35%', 'bottom 0%');
+  // });
+
+  // gsap.matchMedia().add('(min-width: 2441px)', () => {
+  //   scrollAnimation('bottom 40%', 'bottom 0%');
+  // });
 
   //   let tween = gsap.to('.home_header_list', {
   //     yPercent: -170,
