0ff92b72d7273a4b50752197ea48570e88307dfe
Róbert Klimant <roboklimant@gmail.com>
2024-02-15 14:36:25 +0100

    feat: on scroll animation cta

---
+++ src/utils/cta.ts
@@ -0,0 +1,37 @@
+import { gsap } from 'gsap';
+
+export const cta = () => {
+  const pageEl = document.querySelector('.page-wrapper') as HTMLElement;
+  const ctaEl = document.querySelector('[cta-scroll]') as HTMLElement;
+  if (!pageEl || !ctaEl) return;
+
+  ctaEl.style.opacity = '0';
+
+  const pageHeight = pageEl.offsetHeight;
+  const windowHeight = window.innerHeight;
+
+  if (pageHeight + 100 > windowHeight) {
+    window.onscroll = () => {
+      if (window.scrollY > 80) {
+        animationIn();
+      } else {
+        animationOut();
+      }
+    };
+  } else {
+    animationIn();
+  }
+
+  function animationOut() {
+    gsap.to(ctaEl, {
+      opacity: 0,
+      duration: 0.3,
+    });
+  }
+  function animationIn() {
+    gsap.to(ctaEl, {
+      opacity: 1,
+      duration: 0.3,
+    });
+  }
+};
